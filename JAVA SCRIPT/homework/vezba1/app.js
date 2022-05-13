d7bb265160da706bebe2a4d18caf0f7e7f23a601
unknown <merjemhadzijasarevic@gmail.com>
2022-05-13 18:57:08 +0200

    grading students/homework1

---
+++ JAVA SCRIPT/homework/vezba1/app.js
@@ -0,0 +1,13 @@
+ocena = prompt("Unesite broj:");
+
+if (0 < ocena && ocena < 20) {
+  console.log("Ocena je 1");
+} else if (21 < ocena && ocena < 40) {
+  console.log("Ocena je 2 ");
+} else if (41 < ocena && ocena < 60) {
+  console.log("Ocena je 3");
+} else if (61 < ocena && ocena < 80) {
+  console.log("Ocena je 4");
+} else {
+  console.log("Ocena je 5");
+}

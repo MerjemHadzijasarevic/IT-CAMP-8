cab1115afc69dca639c092135457722c0747244c
unknown <merjemhadzijasarevic@gmail.com>
2022-05-27 18:31:22 +0200

    homework/specific characters

---
+++ JAVA SCRIPT/homework/vezba 3/app.js
@@ -0,0 +1,11 @@
+//if contains 65 & 77
+a = 65;
+b = 77;
+
+unos = parseInt(prompt("Unesite broj:"));
+
+if (unos == a || unos == b) {
+  console.log("Broj pronadjen");
+} else {
+  console.log("Broj nije pronadjen");
+}

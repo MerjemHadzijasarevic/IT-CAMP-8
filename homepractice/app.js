687c58ee512c90f01cf9ee8cc3415c50378199f1
unknown <merjemhadzijasarevic@gmail.com>
2022-06-03 19:12:54 +0200

    practice

--- homepractice/app.js
+++ homepractice/app.js
@@ -1,4 +1,4 @@
-broj1 = parseInt(prompt("Unesite prvi broj:"));
+/*broj1 = parseInt(prompt("Unesite prvi broj:"));
 broj2 = parseInt(prompt("Unesite drugi broj"));
 pomocni = 0;
 
@@ -7,3 +7,21 @@ broj2 = pomocni;
 
 console.log(broj1);
 console.log(broj2);
+*/
+
+/*str1 = "This is first " + "This is second";  //concat strings with plus
+console.log(str1);
+
+str2 = "this is first str ";
+str2 += "this is second";
+console.log(str2); 
+
+myname = "Merjem ";
+mysurname = "Hadzijasarevic";
+
+console.log(myname + mysurname);
+*/
+
+/* fname = "Ada";
+lastletter = fname[fname.length - 1];
+console.log(fname.length); */
